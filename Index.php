eccacb92d00cf59895073c28cc4b3fb7420841e3
luluksofiyah <sofiyahluluk7@gmail.com>
2021-10-18 16:55:45 +0700

    Mengedit file Index.php

--- Index.php
+++ Index.php
@@ -1,6 +1,5 @@
 <?php
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
-echo "<h3>Ng. Satria Utomo W.P.(155610024)</h3>"
-
+echo "<h3>Luluk Sofiyah(205410120)</h3>"
 ?>
